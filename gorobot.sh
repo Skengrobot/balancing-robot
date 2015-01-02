f04e368ad16057a8187908eeb6409f996f39cb72
root <root@arm.(none)>
2015-01-02 21:26:20 +0000

    updated gorobot for proper PWM initialization

--- gorobot.sh
+++ gorobot.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+#run as root
 
-# 	run as root
+SLOTS=/sys/devices/bone_capemgr.8/slots 
+
 #	sets pin modes to 7 for digital output for 
 # hbridge directions 
 # All pins are on the P9 header 
 # Pins are mapped to inputs on the H-bridge as:
 # IN1 IN2 IN3 IN4
 # 11  12  13  17
+echo pinctrl-test-0 > $SLOTS
 
 
 # also pins on P9 header
@@ -16,5 +19,14 @@
 modprobe ti_am335x_tscadc
 
 # PWM pins are 14 and 16
-./pwm-enable -e 0 1 2
+modprobe pwm_test
+echo am33xx_pwm > $SLOTS
+echo bone_pwm_P9_14 > $SLOTS
+echo bone_pwm_P9_16 > $SLOTS
+echo 500 > /sys/devices/ocp.3/pwm_test_P9_14.11/period
+echo 500 > /sys/devices/ocp.3/pwm_test_P9_16.12/period
 
+# Make sure that everything is ok
+lsmod
+cat /sys/kernel/debug/pwm
+cat $SLOTS
