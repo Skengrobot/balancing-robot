21bab08a6769b0f671a4acc9ae92b4e432a0a735
Rudi Plesch <rudi.plesch@gmail.com>
2015-01-02 14:21:20 -0800

    proper proper array iteration

--- gorobot.sh
+++ gorobot.sh
@@ -12,7 +12,7 @@ SLOTS=/sys/devices/bone_capemgr.8/slots
 # OS gpio number: 30  60  31   5
 echo pinctrl-test-0 > $SLOTS
 gpio_outputs=
-for pin in $( 30 60 31 5 );
+for pin in 30 60 31 5 ;
 do
     echo $pin > /sys/class/gpio/export
     echo out > /sys/class/gpio$pin/direction
